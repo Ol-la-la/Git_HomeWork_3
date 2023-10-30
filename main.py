388fdd303124dab6e5144705c4d65972ea9d623f
Olla <oldansh@yandex.ru>
2023-10-30 21:34:22 +0300

    Add get_list_data_dict function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from processing import  get_list_dict
+from processing import get_list_data_dict, get_list_dict
 
 if __name__ == '__main__':
     user_dicts = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -6,3 +6,4 @@ if __name__ == '__main__':
                   {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                   {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
     print(get_list_dict(user_dicts))
+    print(get_list_data_dict(user_dicts))
--- processing.py
+++ processing.py
@@ -4,3 +4,12 @@ def get_list_dict(user_dicts: list[dict], state_param: str = 'EXECUTED') -> list
         if user_dict['state'] == state_param:
             new_dict.append(user_dict)
     return new_dict
+
+
+def get_list_data_dict(user_dicts: list[dict], sort_order: bool = False) -> list[dict]:
+    new_dict: list[dict] = []
+    if sort_order:
+        new_dict = sorted(user_dicts, key=lambda x: x['date'])
+    else:
+        new_dict = sorted(user_dicts, key=lambda x: x['date'], reverse=True)
+    return new_dict
