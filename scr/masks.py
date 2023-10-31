7e31e2b32879c9200e326e98b817e326f920ddf3
Olla <oldansh@yandex.ru>
2023-10-31 10:35:02 +0300

    Add widget.py and masks.py

---
+++ scr/masks.py
@@ -0,0 +1,14 @@
+def get_mask_card(list_of_data: str) -> str:
+    """
+    Получаем номер карты и выводим в нужном формате
+    """
+    number_card = list_of_data[-1]
+    card_name = " ".join(list_of_data[0:-1])
+    return f"{card_name} {number_card[:4]} {number_card[5:7]}** **** {number_card[-4:]}"
+
+
+def get_mask_score(number_score: str) -> str:
+    """
+    Получаем номер карты и выводим в нужном формате
+    """
+    return f"Счет **{number_score[-4:]}"
---
+++ scr/widget.py
@@ -0,0 +1,23 @@
+from scr.masks import get_mask_card, get_mask_score
+
+
+def checking_number(name_number_card: str) -> str:
+    """
+    Получаем исходные данные с номером карты или счета.
+    Преобразовываем их в нужный нам формат.
+    """
+    list_of_data = name_number_card.split()
+    number_card = list_of_data[-1]
+    if len(number_card) == 20:
+        return get_mask_score(number_card)
+    elif len(number_card) == 16:
+        return get_mask_card(list_of_data)
+    else:
+        return "Введены неверные данные"
+
+
+def get_datatime(data_time: str) -> str:
+    """
+    Преобразовывает дату в нужный формат
+    """
+    return f"{data_time[8:10]}.{data_time[5:7]}.{data_time[:4]}"
