32e1d82c14f3719add6040bc40bca0a5f85f5623
Olla <oldansh@yandex.ru>
2023-10-31 22:10:27 +0300

    Add docstring, add linters

--- scr/processing.py
+++ scr/processing.py
@@ -1,4 +1,12 @@
 def get_list_dict(user_dicts: list[dict], state_param: str = "EXECUTED") -> list[dict]:
+    """
+    Функция, которая принимает на вход список словарей.
+    Возвращает новый список, содержащий только те словари,
+    у которых ключ state соответствует заданному значению.
+    :param user_dicts: Исходный список словарей
+    :param state_param: нужное значение ключа state, по умолчанию EXECUTED
+    :return: новый список словарей
+    """
     new_dict: list[dict] = []
     for user_dict in user_dicts:
         if user_dict["state"] == state_param:
@@ -7,7 +15,13 @@ def get_list_dict(user_dicts: list[dict], state_param: str = "EXECUTED") -> list
 
 
 def get_list_data_dict(user_dicts: list[dict], sort_order: bool = False) -> list[dict]:
-    new_dict: list[dict] = []
+    """
+    Функция, которая принимает на вход список словарей.
+    Возвращает новый список, отсортированный по дате
+    :param user_dicts: Исходный список словарей
+    :param sort_order: способ сортировки даты (по умолчанию - по возрастанию)
+    :return: новый список словарей
+    """
     if sort_order:
         new_dict = sorted(user_dicts, key=lambda x: x["date"])
     else:
