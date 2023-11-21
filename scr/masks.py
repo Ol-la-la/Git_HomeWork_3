d3d868a8ec3093b2c38b755ff280ed08422e6a51
Olla <oldansh@yandex.ru>
2023-11-21 22:49:35 +0300

    Add log_utils and log_masks function

--- scr/masks.py
+++ scr/masks.py
@@ -1,9 +1,14 @@
+import logging
+from typing import Any
+
+
 def get_mask_card(list_of_data: str) -> str:
     """
     Получаем номер карты и выводим в нужном формате
     """
     number_card = list_of_data.split()[-1]
     card_name = " ".join(list_of_data.split()[0:-1])
+    log_masks('Номер карты обработан', 'INFO')
     return f"{card_name} {number_card[:4]} {number_card[5:7]}** **** {number_card[-4:]}"
 
 
@@ -11,4 +16,28 @@ def get_mask_score(number_score: str) -> str:
     """
     Получаем номер карты и выводим в нужном формате
     """
+    log_masks('Номер счета обработан', 'INFO')
     return f"Счет **{number_score[-4:]}"
+
+
+def log_masks(log_message: str, log_level: str) -> Any:
+    logger = logging.getLogger(__name__)
+    file_handler = logging.FileHandler('masks.log', 'w', encoding="utf-8")
+    file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+    if log_level == "DEBUG":
+        logger.setLevel(logging.DEBUG)
+        return logger.debug(log_message)
+    elif log_level == "INFO":
+        logger.setLevel(logging.INFO)
+        return logger.info(log_message)
+    elif log_level == "WARNING":
+        logger.setLevel(logging.WARNING)
+        return logger.warning(log_message)
+    elif log_level == "ERROR":
+        logger.setLevel(logging.ERROR)
+        return logger.error(log_message)
+    elif log_level == "CRITICAL":
+        logger.setLevel(logging.CRITICAL)
+        return logger.critical(log_message)
--- scr/utils.py
+++ scr/utils.py
@@ -1,3 +1,4 @@
+import logging
 import json
 from typing import Any
 
@@ -10,10 +11,13 @@ def get_operations_json(path_name: str) -> Any:
     try:
         with open(path_name, encoding="utf-8") as f:
             data = json.load(f)
+            log_utils('Файл считывается корректно', 'INFO')
     except FileNotFoundError:
         print(f"File {path_name} not found.")
         data = []
+        log_utils('Вы пытаетесь обратиться к несуществующему файлу', 'ERROR')
     except json.decoder.JSONDecodeError:
+        log_utils('Ошибка формата файла', 'ERROR')
         print(f"File {path_name} is not json")
         data = []
     return data
@@ -26,7 +30,33 @@ def transaction_currency(transaction: dict) -> float | Any:
     :return: float
     """
     if transaction["operationAmount"]["currency"]["code"] == "RUB":
+        log_utils('Функция работает корректно', 'INFO')
         s = transaction["operationAmount"]["amount"]
         return s
     else:
+        log_utils('Произошла ошибка транзакции. Транзакция выполнена не в рублях', 'ERROR')
         raise ValueError("Транзакция выполнена не в рублях. Укажите транзакцию в рублях")
+
+
+def log_utils(log_message: str, log_level: str) -> Any:
+    logger = logging.getLogger(__name__)
+    file_handler = logging.FileHandler('utils.log', 'w', encoding="utf-8")
+    file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+    logger.propagate = True
+    if log_level == "DEBUG":
+        logger.setLevel(logging.DEBUG)
+        return logger.debug(log_message)
+    elif log_level == "INFO":
+        logger.setLevel(logging.INFO)
+        return logger.info(log_message)
+    elif log_level == "WARNING":
+        logger.setLevel(logging.WARNING)
+        return logger.warning(log_message)
+    elif log_level == "ERROR":
+        logger.setLevel(logging.ERROR)
+        return logger.error(log_message)
+    elif log_level == "CRITICAL":
+        logger.setLevel(logging.CRITICAL)
+        return logger.critical(log_message)
